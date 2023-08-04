fdab1e91f6ef9e2fbe8a696fc789c999b373d9f7
lululufr <lucas.lm.miller@gmail.com>
2023-08-04 18:39:30 +0200

    first style - from templates

--- src/resources/views/components/footer.blade.php
+++ src/resources/views/components/footer.blade.php
@@ -1,7 +1,40 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
+
+<footer id="footer" class="jumbotron">
+    <section class="container">
+        <div class="row">
+            <div class="col-md-5 col-md-push-1">
+                <h2>Got your interest?</h2>
+                <p>Please tell your friends about SuperAwesome!</p>
+                <!-- AddThis Button BEGIN -->
+                <div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+                    <a class="addthis_button_facebook"></a>
+                    <a class="addthis_button_twitter"></a>
+                    <a class="addthis_button_linkedin"></a>
+                    <a class="addthis_button_google_plusone_share"></a>
+                    <a class="addthis_button_compact"></a><a class="addthis_counter addthis_bubble_style"></a>
+                </div>
+                <script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+                <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-536ba34f3dab1f93"></script>
+                <!-- AddThis Button END -->
+
+            </div>
+            <div class="col-md-5 col-md-push-1">
+                <h2>Need help?</h2>
+                <p>For help with this or another template you've downloaded from GetTemplate, please email me at gt@gettemplate.com. </p>
+                <p>If you'd like to report a bug or suggest an improvement, please post and issue on <a href="https://github.com/pozh/">GitHub</a> </p>
+            </div>
+        </div>
+    </section>
+</footer>
+
+<br>
+
+
+
+<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="{{asset('/js/app.js')}}"></script>
 
 </body>
 </html>
--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -1,15 +1,101 @@
 <x-header/>
+<!-- Header -->
+<header class="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-push-2 text-center">
+                <h1>Regles Solidaires</h1>
+                <h2>Amiens Zher</h2>
+                <p class="lead">
+                    Pikachu diablo 3 asians angry birds final week varius le girlfriend in.
+                    Feel Like a Sir panda blizzard hac mom nerd monday in on nyan cat.
+                </p>
 
-<h1>Hello, world!</h1>
+                <p>SuperAwesome thing is currently in active development and will be released this summer.<br>
+                    In the meantime, please register to our waiting list to get notified first once something is launched.</p>
+                <br>
+            </div>
+        </div>
 
-<button type="button" class="btn btn-primary">Primary</button>
-<button type="button" class="btn btn-secondary">Secondary</button>
-<button type="button" class="btn btn-success">Success</button>
-<button type="button" class="btn btn-danger">Danger</button>
-<button type="button" class="btn btn-warning">Warning</button>
-<button type="button" class="btn btn-info">Info</button>
-<button type="button" class="btn btn-light">Light</button>
-<button type="button" class="btn btn-dark">Dark</button>
+        <nav class="nav nav-pills flex-column flex-sm-row bg-dark">
+            <a class="flex-sm-fill text-sm-center nav-link active" aria-current="page" href="#">Active</a>
+            <a class="flex-sm-fill text-sm-center nav-link" href="#">Longer nav link</a>
+            <a class="flex-sm-fill text-sm-center nav-link" href="#">Link</a>
+            <a class="flex-sm-fill text-sm-center nav-link disabled" aria-disabled="true">Disabled</a>
+        </nav>
+
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div id="illustration">
+                    <img src="{{asset('/img/logo.png')}}" class="img-fluid w-50" alt="" >
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- /Header -->
+
+
+<!-- Content -->
+<main class="content">
+
+    <!-- Lead -->
+    <section class="container space-before space-after">
+        <div class="row">
+            <div class="col-sm-10 col-sm-push-1">
+                <h1 class="text-center">What it's all about</h1>
+                <p class="lead text-center">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nam mi odio, gravida at turpis et, porta bibendum justo.
+                </p>
+            </div>
+        </div>
+    </section>
+    <!-- /Lead -->
+
+
+
+    <!-- Features -->
+    <section class="container space-before">
+
+        <div class="row featurelist space-after">
+            <div class="col-md-5 col-sm-6 col-md-push-1 ">
+                <img class="img-feature img-responsive" src="{{asset('/img/DSCF2276.JPEG')}}" alt="Sample image">
+            </div>
+            <div class="col-md-5 col-md-push-1 col-sm-6">
+                <h2 class="space-before">The template is quite small, <span class="text-muted">so html code is simple</span></h2>
+                <p>What you have to ask yourself is, do I feel lucky. well do ya' punk? Ever notice how sometimes you come across somebody you shouldn't have f**ked with? Well, I'm that guy. Dyin' ain't much of a livin', boy.</p>
+            </div>
+        </div>
+
+        <div class="row featurelist space-after">
+            <div class="col-md-5 col-sm-6 col-sm-push-6">
+                <img class="img-feature img-responsive" src="{{asset('/img/DSCF2228.JPEG')}}" alt="Sample image">
+            </div>
+            <div class="col-md-5 col-sm-6 col-md-pull-4 col-sm-pull-6">
+                <h2 class="space-before">Yes, I love cool backgrounds, <span class="text-muted">like the one used here</span></h2>
+                <p>There are 2 more color themes inside this template. You'll just need to uncomment
+                    few lines in the CSS file to activate any of them. </p>
+                <p>Now, continue with our lipsum :)
+                    This is the ak-47 assault rifle, The preferred weapon of your enemy; and it makes a distinctive sound when
+                    fired at you, so remember it. When a naked man's chasing a woman through an alley with a butcher knife and a hard-on, I figure he's not out collecting for the red cross. well, do you have anything to say for yourself? </p>
+            </div>
+        </div>
+
+        <div class="row featurelist space-after">
+            <div class="col-md-5 col-sm-6 col-md-push-1 col-sm-6">
+                <img class="img-feature img-responsive" src="{{asset('/img/DSC0492.png')}}" alt="Sample image">
+            </div>
+            <div class="col-md-5 col-md-push-1 col-sm-6">
+                <h2 class="space-before">Violet does look good too!</span></h2>
+                <p>Here. Put that in your report! And "I may have found a way out of here. you want a guarantee, buy a toaster. are you feeling lucky punk dyin' ain't much of a livin', boy. what you have to ask yourself is, Do I feel lucky. Well do ya' punk? You see, in this world there's two kinds of people, my friend: those with loaded guns and those who dig. you dig.</p>
+            </div>
+        </div>
+    </section>
+    <!-- /Features -->
+
+</main>
 
 
 <x-footer/>
